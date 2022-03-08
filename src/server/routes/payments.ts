a2462b849a537e5ae8b1e13f51d3fc5ade43cb33
eitay <eitay@soos.org.il>
2022-03-08 19:40:22 +0200

    working payments router

--- src/server/routes/payments.ts
+++ src/server/routes/payments.ts
@@ -5,19 +5,80 @@ import {auth} from "../middlewares/auth";
 
 
 /**
- * @route   POST /payment
+ * @route   POST /payments
  * @desc    create new payment
  * @access  public
  * */
+//auth
+//x-auth-token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIyMzQ5MThhODVjZDJiYjRhZDYwOTU3In0sImlhdCI6MTY0Njc2MDAzMCwiZXhwIjoxNjQ3MTIwMDMwfQ.xrphyln9kX7asZfTBa4sc3uzmmkRtzB7q72Rq-m86GI"
 router.post('/', auth, async (req: Request, res: Response)=>{
     const {apartmentID, user, amount} = req.body;
     if (amount <= 0){
         return res.status(400).json({msg: 'Amount cant be equal or below zero'});
     }
     try{
-        await Payment.create({})
+        await new Payment({apartmentID, user, amount}).save();
+        res.status(200).json({msg: 'Payment added successfully'});
+    }catch (err) {
+        if(err){
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
     }
+})
 
+/**
+ * @route   PATCH /payments/:id
+ * @desc    create new payment
+ * @access  public
+ * */
+router.patch('/:id', auth, async (req: Request, res: Response)=>{
+    const {user, amount} = req.body;
+    try{
+        const payment = await Payment.findById(req.params.id);
+        if (!payment){
+            return res.status(404).json({msg: 'Payment has not found'});
+        }
+        if (amount <= 0){
+            return res.status(400).json({msg: 'Amount cant be equal or below zero'});
+        }
+        if (payment.user.toString() !== user){
+            return res.status(401).json({msg: 'Not authorized to edit this payment'});
+        }
+        payment.amount = amount;
+        await payment.save();
+        res.status(200).json({msg: 'Payment edited successfully'});
+    }catch (err) {
+        if(err){
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+})
+
+/**
+ * @route   DELETE /payments/:id
+ * @desc    create new payment
+ * @access  public
+ * */
+router.delete('/:id', auth, async (req: Request, res: Response)=>{
+    const {user} = req.body;
+    try{
+        const payment = await Payment.findById(req.params.id);
+        if (!payment){
+            return res.status(404).json({msg: 'Payment has not found'});
+        }
+        if (payment.user.toString() !== user){
+            return res.status(401).json({msg: 'Not authorized to edit this payment'});
+        }
+        await payment.remove();
+        res.status(200).json({msg: 'Payment removed successfully'});
+    }catch (err) {
+        if(err){
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
 })
 
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,12 +3,14 @@ const app:Application = express();
 const port: number = process.env.PORT? parseInt(process.env.PORT) : 5000;
 import {connectToMongo} from "./utils/mongodb";
 import user from './routes/user'
+import payments from "./routes/payments";
 
 
 app.use(express.json());
 connectToMongo();
 
 app.use('/user', user);
+app.use('/payments', payments);
 
 app.listen(port, ()=>{
     console.log(`server listening on ${port}`);
